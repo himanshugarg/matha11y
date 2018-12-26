a021bc5a6efe74af42d90eb90af9017cd61c932d
Himanshu Garg <himanshugarg@zoho.com>
2018-12-26 17:46:42 +0530

    run mathjax after latex

--- abp/content.js
+++ abp/content.js
@@ -20,18 +20,29 @@ function main() {
       xhr = new XMLHttpRequest();
       xhr.onload = function() {
         let json = JSON.parse(xhr.response);        
-        if (json.latex_confidence_rate > 0.99) {          
-          console.log(json);
-          let text = document.createTextNode(json.latex);
-          image.parentNode.replaceChild(text, image);
-        } 
+        if (json.latex_confidence_rate > 0.90) {                    
+          let text = document.createTextNode('\\(' + json.latex + '\\)');
+          image.parentNode.replaceChild(text, image);          
+        } else {
+          alert(xhr.response);
+        }
       };
       xhr.open("POST", "https://api.mathpix.com/v3/latex");
       xhr.setRequestHeader('app_id', 'himanshugarg_zoho_com');
-      xhr.setRequestHeader('app_key', '<api_key>');
+      xhr.setRequestHeader('app_key', '<api_key_here>');
       xhr.setRequestHeader('Content-type', 'application/json');
       xhr.send(`{"src": "${base64}"}`);      
     });
-  }  
+  }
+  loadMathJax();  
 }
+
+const loadMathJax = () => {
+  var script = document.createElement("script");
+    script.type = "text/javascript";
+    script.src  = "https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.5/latest.js?config=TeX-MML-AM_CHTML";
+    document.getElementsByTagName("head")[0].appendChild(script);
+    alert("MathJax loaded");
+  };
+
 main();
